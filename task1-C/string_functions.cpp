7263497116144e481516f0199a6cb99a8b323363
antonkulikov401 <anton.kulikov401@gmail.com>
2019-11-24 01:15:46 +0300

    changed GetSubpatterns function

--- task1-C/string_functions.cpp
+++ task1-C/string_functions.cpp
@@ -2,32 +2,32 @@
 #include "trie.hpp"
 #include <unordered_map>
 #include <algorithm>
-#include <string_view>
 
 std::vector<std::pair<std::string_view, std::vector<size_t>>>
-GetSubpatterns(std::string& pattern) {
-    if (pattern[pattern.size() - 1] != '?')
-        pattern += '?';
+GetSubpatterns(std::string_view pattern) {
     std::unordered_map<std::string_view, std::vector<size_t>> subpatterns;
-    size_t subpatternBegin = 0;
-    size_t index = 0;
-    for (size_t subpatternEnd = 0; subpatternEnd < pattern.size();
-            ++subpatternEnd) {
-        if (pattern[subpatternEnd] == '?') {
-            if (pattern[subpatternBegin] != '?')
-                subpatterns[std::string_view(&pattern[subpatternBegin],
-                    subpatternEnd - subpatternBegin)].push_back(index - 1);
-            subpatternBegin = subpatternEnd + 1;
+    auto curr = pattern.begin();
+    auto begin = pattern.begin();
+    while (curr != pattern.end()) {
+        if (*curr != '?') {
+            ++curr;
+            continue;
         }
-        ++index;
+        auto lastChar = curr;
+        while (*curr == '?') ++curr;
+        subpatterns[std::string_view(begin,
+            lastChar - begin)].push_back(begin - pattern.begin());
+        begin = curr;
     }
+    if (begin != pattern.end())
+        subpatterns[std::string_view(begin,
+            pattern.end() - begin)].push_back(begin - pattern.begin());
     return std::vector<std::pair<std::string_view,
         std::vector<size_t>>>(subpatterns.begin(), subpatterns.end());
 }
 
-std::vector<size_t> FindPattern(const std::string& _pattern,
+std::vector<size_t> FindPattern(std::string_view pattern,
         const std::string& text) {
-    std::string pattern = _pattern;
     std::vector<size_t> indices;
     auto subpatterns = GetSubpatterns(pattern);
     std::vector<std::string_view> subpatternStrings;
@@ -51,7 +51,7 @@ std::vector<size_t> FindPattern(const std::string& _pattern,
 
     for (size_t i = 0; i < text.size(); ++i)
         if (patternEntries[i] == numOfSubpatterns &&
-            (i + _pattern.size() <= text.size()))
+            (i + pattern.size() <= text.size()))
             indices.push_back(i);
     return indices;
 }
--- task1-C/string_functions.hpp
+++ task1-C/string_functions.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <string_view>
 
-std::vector<size_t> FindPattern(const std::string& pattern,
+std::vector<size_t> FindPattern(std::string_view pattern,
     const std::string& text);
 
 std::vector<std::string> Split(const std::string& str, char delimeter);
